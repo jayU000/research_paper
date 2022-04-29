db9c70f893760b37f2a597fe5db53270ebdf51cd
jayU000 <wjdghk7528@naver.com>
2022-04-29 09:57:28 +0900

    초기 설정

---
+++ src/main/java/com/example/research_paper/ResearchPaperApplication.java
@@ -0,0 +1,13 @@
+package com.example.research_paper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchPaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchPaperApplication.class, args);
+	}
+
+}
